abea2cc893c6c8f5c9398e29157ac633e317b8b3
Chang-Hai <864744977@qq.com>
2022-06-26 23:41:24 +0800

    提交

---
+++ demo/hooks-demo/src/useApi.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+
+const getList = (query) => {
+    return new Promise((resolve,reject) => {
+        setTimeout(()=> {
+            console.log('query',query)
+            resolve([6,7,8,9,10])
+        }, 3000)
+    })
+}
+
+const useApi = () =>{
+    const [data, setData] = useState([1, 2, 3, 4, 5])
+    const [query, setQuery] = useState('')
+
+    useEffect(() => {
+        (async () => {
+            const data = await getList()
+            console.log('data', data)
+            setData(data)
+        })()
+    }, [query])
+
+    return [{ data }, setQuery]
+}
+export default useApi
